d1651c5e1568b95973a322457b63be57d7dbf1c7
Kimeshan Naidoo <kimeshann@soliditech.com>
2025-09-21 22:33:00 +0200

    Added Keyboard

--- keyboards/cornetrackball/charybdis/3x6/config.h
+++ keyboards/cornetrackball/charybdis/3x6/config.h
@@ -22,7 +22,7 @@
 #define ROTATIONAL_TRANSFORM_ANGLE -25
 
 /* Handedness. */
-
+#define USB_VBUS_PIN GP19
 
 // To use the handedness pin, resistors need to be installed on the adapter PCB.
 // If so, uncomment the following code, and undefine MASTER_RIGHT above.
